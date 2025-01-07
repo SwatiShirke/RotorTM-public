9d8a81f9d15df1a67991f1e754951beb849161be
Swati Shirke <swatishirke.shirke88@gmail.com>
2025-01-07 12:04:52 -0500

    migrated the repository from ROS1 to ROS2

--- rotor_tm_control/setup.py
+++ rotor_tm_control/setup.py
@@ -1,11 +1,27 @@
-#!/usr/bin/env python
+from setuptools import find_packages, setup
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+package_name = 'rotor_tm_control'
 
-setup_args = generate_distutils_setup(
-    packages=['rotor_tm_control'],
-    package_dir={'': 'src'},
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='swati',
+    maintainer_email='swatishirke.shirke88@gmail.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'controller_node = rotor_tm_control.controller_node:main'
+
+        ],
+    },
 )
-
-setup(**setup_args)
